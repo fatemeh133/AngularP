72938ebaf48880f509512ae0f515ce81322931e5
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-28 09:02:45 +0330

    initial component and stuff

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css',
+})
+export class TestComponent {}
