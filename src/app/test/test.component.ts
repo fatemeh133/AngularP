5507a1381be54279df58b4179bb427ed0f0b197b
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-01-28 16:24:51 +0330

    bindings and stuff

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './test.component.html',
   styleUrl: './test.component.css',
 })
-export class TestComponent {}
+export class TestComponent {
+  id: number = 23;
+  name: string = 'fat';
+  btnDisabled: boolean = true;
+  inputType: string = 'text';
+  imgUrl: string =
+    'https://us.123rf.com/450wm/cc0collection/cc0collection2205/cc0collection220540385/186116070-dark-room-people-woman-girl-photography-photographer-camera-lens-outdoor-travel.jpg?ver=6';
+  constructor() {
+    setTimeout(() => {
+      {
+        this.btnDisabled = false;
+      }
+    }, 2000);
+  }
+  getID() {
+    return this.id;
+  }
+  clickEvent() {
+    alert('clicked');
+  }
+  logValue(event: Event) {
+    console.log((event.target as HTMLInputElement).value);
+  }
+}
